d830e119e1904db4eb6e6a42b2ecc841a56932b5
JaPNaA <leonehuang100@gmail.com>
2018-11-20 18:45:48 -0500

    add help doc for japnaa, fix random bug

--- plugins/japnaa.js
+++ plugins/japnaa.js
@@ -1,5 +1,6 @@
 const BotPlugin = require("../src/plugin.js");
 const BotCommandOptions = require("../src/botcommandOptions.js");
+const BotCommandHelp = require("../src/botcommandHelp.js");
 const Logger = require("../src/logger.js");
 const { stringToArgs, random, toUserId } = require("../src/utils.js");
 
@@ -99,7 +100,7 @@ class Japnaa extends BotPlugin {
         const args = stringToArgs(argString);
 
         // !random string
-        if (args[0].toLowerCase() == "string") {
+        if (args[0] && args[0].toLowerCase() == "string") {
             bot.send(event.channelId, 
                 "```" + 
                 this._randomString()
@@ -453,15 +454,116 @@ class Japnaa extends BotPlugin {
     }
 
     _start() {
-        this._registerCommand("echo", this.echo);
-        this._registerCommand("count", this.count);
-        this._registerCommand("random", this.random);
-        this._registerCommand("spam", this.spam_command, new BotCommandOptions({
-            noDM: true
+        this._registerCommand("echo", this.echo, new BotCommandOptions({
+            help: new BotCommandHelp({
+                description: "Says what you say, you can also echo JSON objects.",
+                overloads: [{
+                    "string": "Echos back what you said"
+                }, {
+                    "JSON string": "Parses JSON string, and echos it back. See discord.io `send` method docs for more information."
+                }],
+                examples: [
+                    ["echo hi", "The bot will respond with \"hi\", so you're not left hanging."],
+                    ["echo {\"embed\": {\"color\": 589253, \"title\": \"JSON!\", \"description\": \"JavaScript Object Notation\"}}", 
+                        "Responds with an embed with a cyan-ish color, the title \"JSON\", and the description \"JavaScript Object Notation\""]
+                ]
+            })
         }));
-        this._registerCommand("throw", this.throw);
-        this._registerCommand("play", this.play);
-        this._registerCommand("tell", this.tell);
+
+        this._registerCommand("count", this.count, new BotCommandOptions({
+            help: new BotCommandHelp({
+                description: "Increments a counter in the bot by 1, and sends it.",
+                examples: [
+                    ["count", "Will respond with a number bigger than what it reponded to the previous count count."]
+                ]
+            })
+        }));
+
+        this._registerCommand("random", this.random, new BotCommandOptions({
+            help: new BotCommandHelp({
+                description: "Generates a random thing",
+                overloads: [{
+                    "[max]": "Optional. The maximum of the random number",
+                    "[min]": "Optional. The minimum of the random number",
+                    "[step]": "Optional. What the number must be dividible by. 0 indicates it doesn't have to be divisible by anything."
+                }, {
+                    "\"string\"": "\"string\", will respond with a randomly generated 128 character string."
+                }],
+                examples: [
+                    ["random", "A random number between 0 to 1 with no step"],
+                    ["random 5", "A random number between 0 to 5 that's divisible by 1"],
+                    ["random 5 10", "A random number between 5 and 10 that's divisible by 1"],
+                    ["random 5 10 2", "A random number between 5 and 10 that's divisible by 2"],
+                    ["random 5 10 1.6", "A random number between 5 and 10 that's divisible by 1.6"],
+                    ["random string", "A random string 128 characters long"]
+                ]
+            })
+        }));
+
+        this._registerCommand("spam", this.spam_command, new BotCommandOptions({
+            noDM: true,
+            help: new BotCommandHelp({
+                description: "Spams a message several times, because you want to be annoying.",
+                overloads: [{
+                    "[amount]": "Optional. The amount of spam to spam. Defaults to 3.",
+                    "[use counter]": "Optional. Adds a counter to your spam. Defaults to none.",
+                    "...message": "The message to spam"
+                }, {
+                    "\"stop\"": "Stops all spam on the server"
+                }, {
+                    "\"stop all\"": "Stops all spam on all servers"
+                }, {
+                    "\"limit\"": "Sends the spam limit for that location"
+                }, {
+                    "\"que limit\"": "Sends the spam que limit for that server"
+                }],
+                examples: [
+                    ["spam cows", "Spams \"cow\" 3 times."],
+                    ["spam 5 cows", "Spams \"cow\" 5 times."],
+                    ["spam 5 true cows", "Adds a counter to the \"\""],
+                    ["spam stop", "Stops all spam on the server"],
+                    ["spam stop all", "Stops all spam on all servers (requires `BOT_ADMINISTRATOR`)"],
+                    ["spam limit", "Responds with the limit of the spam of the channel"],
+                    ["spam que limit", "Responds with the que limit of the channel"]
+                ]
+            })
+        }));
+        this._registerCommand("throw", this.throw, new BotCommandOptions({
+            help: new BotCommandHelp({
+                description: "Throws an error.",
+                overloads: [{
+                    "[errorName]": "The name of the error to throw"
+                }],
+                examples: [
+                    ["throw", "The bot will throw a User-Throw Error"],
+                    ["throw rocks", "The bot will throw the error \"rocks\""]
+                ]
+            })
+        }));
+        this._registerCommand("play", this.play, new BotCommandOptions({
+            help: new BotCommandHelp({
+                description: "Sets the \"playing\" value",
+                overloads: [{
+                    "value": "The \"game\" to \"play\""
+                }],
+                examples: [
+                    ["play", "Removes the \"playing\" tag"],
+                    ["play nothing", "Sets the \"playing\" tag to \"nothing\"."]
+                ]
+            })
+        }));
+        this._registerCommand("tell", this.tell, new BotCommandOptions({
+            help: new BotCommandHelp({
+                description: "The bot sends a direct message to the user, telling the user that you told them something.",
+                overloads: [{
+                    "user": "The userId or @mention, user to send message to.",
+                    "...message": "The message to send th user above."
+                }],
+                examples: [
+                    ["tell <@207890448159735808> hi", "The bot will send <@207890448159735808> a message, and include a not that you sent it."]
+                ]
+            })
+        }));
     }
 }
 
