9e9dbd36cb7f3182f77aabfcd1d02eaf700d75b0
JaPNaA <leonehuang100@gmail.com>
2018-12-08 12:55:53 -0500

    refactor botcommand.js

--- src/botcommand.js
+++ src/botcommand.js
@@ -6,6 +6,7 @@
  */
 
 const Logger = require("./logger.js");
+const { inspect } = require("util");
 
 class BotCommand {
     /**
@@ -47,58 +48,131 @@ class BotCommand {
          * The word that triggers the command
          */
         this.commandName = commandName.toLowerCase();
+
+        /**
+         * Regex matches 1 whitespace
+         */
+        this.whitespaceRegex = /\s/;
+    }
+
+    /**
+     * @typedef {Object} CleanCommandContent
+     * @property {String} commandContent command trimmed
+     * @property {String} args arguments
+     * @property {String} nextCharAfterCommand next character after the command
+     */
+    /**
+     * Returns cleaned command content
+     * @param {String} dirtyContent dirty content to be cleaned
+     * @returns {CleanCommandContent} cleaned command content
+     */
+    _getCleanCommandContent(dirtyContent) {
+        let trimmed = dirtyContent.trimLeft();
+        let args = trimmed.slice(this.commandName.length + 1); // the +1 is the space after the command
+        let commandContent = trimmed.toLowerCase();
+        let nextCharAfterCommand = commandContent[this.commandName.length];
+
+        return {
+            commandContent: commandContent,
+            args: args,
+            nextCharAfterCommand: nextCharAfterCommand
+        };
+    }
+
+    /**
+     * @typedef {Object} TestResults
+     * @property {Boolean} canRun can the command run?
+     * @property {String} [reasonCannotRun] reason why the command cannot run
+     */
+    /**
+     * Tests if command can be run
+     * @param {DiscordCommandEvent} commandEvent the event to test
+     * @returns {TestResults} Error string
+     */
+    test(commandEvent) {
+        let cleanCommandContent = this._getCleanCommandContent(commandEvent.commandContent);
+
+        if (
+            cleanCommandContent.commandContent.startsWith(this.commandName) && 
+            this.whitespaceRegex.test(cleanCommandContent.nextCharAfterCommand)
+        ) {
+            let permissions = this.bot.getPermissions_channel(commandEvent.userId, commandEvent.serverId, commandEvent.channelId);
+
+            if (this.noDM && !this.bot.getChannel(commandEvent.channelId)) {
+                return {
+                    canRun: false,
+                    reasonCannotRun: "You cannot run this command in Direct Messages"
+                };
+            }
+
+            if (this.requiredPermission && !permissions.has(this.requiredPermission)) {
+                return {
+                    canRun: false,
+                    reasonCannotRun: "<@" + commandEvent.userId + "> **You must have `" +
+                        this.requiredPermission + "` permissions to run this command.**"
+                };
+            }
+
+            return { canRun: true };
+        }
+
+        return { canRun: false };
     }
 
     /**
      * Tests if the commandWord matches, and runs the command if it does.
      * @param {DiscordCommandEvent} commandEvent the event triggering function
-     * @returns {Boolean} Was the command was ran
+     * @returns {Boolean} Did the command run OR not have enough permissions to run
      */
     testAndRun(commandEvent) {
-        let commandContent = commandEvent.commandContent;
-        let commandContentClean = commandContent.toLowerCase().trimLeft();
-
-        if (commandContentClean.startsWith(this.commandName)) {
-            let permissions = this.bot.getPermissions_channel(commandEvent.userId, commandEvent.serverId, commandEvent.channelId);
-            let argString = commandContent.slice(this.commandName.length);
-            
-            if (this.noDM && !this.bot.getChannel(commandEvent.channelId)) {
-                this.bot.send(commandEvent.channelId,
-                    "You cannot run this command in Direct Messages"
-                );
-                return true;
-            }
-
-            if (this.requiredPermission && !permissions.has(this.requiredPermission)) {
-                this.bot.send(commandEvent.channelId, 
-                    "<@" + commandEvent.userId + "> **You must have `" +
-                    this.requiredPermission + "` permissions to run this command.**"
-                );
-                return true;
-            }
-
-            this.tryRunCommand(commandEvent, argString.trimLeft());
-
+        let results = this.test(commandEvent);
+        if (results.canRun) {
+            let cleaned = this._getCleanCommandContent(commandEvent.commandContent);
+            this.tryRunCommand(commandEvent, cleaned.args);
             return true;
-        }
+        } else {
+            if (results.reasonCannotRun) {
+                this.bot.send(commandEvent.channelId, results.reasonCannotRun);
+                return true;
+            }
 
-        return false;
+            return false;
+        }
     }
 
+    /**
+     * Sends an error message
+     * @param {DiscordCommandEvent} commandEvent command event
+     * @param {String} argString arguments as string
+     * @param {Error} error error to send
+     */
+    sendError(commandEvent, argString, error) {
+        let errorStack = error.stack;
+        errorStack = errorStack.split(__dirname).join(".");
+        let message =
+            "```An error occured\n" + errorStack +
+            "\nCommand: " + this.commandName +
+            "\nArguments: " + argString +
+            "\nEvent: " + inspect(commandEvent, { depth: 3 });
+
+        message = message.replace(/ {4}/g, "\t");
+        message = message.slice(0, 1997) + "```";
+
+        this.bot.send(commandEvent.channelId, message);
+
+        Logger.warn(message);
+    }
+
+    /**
+     * Tries to run command, and sends an error message if fails
+     * @param {DiscordCommandEvent} commandEvent command event
+     * @param {String} argString arguments as string
+     */
     tryRunCommand(commandEvent, argString) {
         try {
             this.func(this.bot, commandEvent, argString);
-        } catch (e) {
-            /** @type {String} */
-            let msg = e.stack;
-
-            msg = msg.split(__dirname).join(".");
-
-            this.bot.send(commandEvent.channelId,
-                "```An error occured\n" + msg + "```"
-            );
-
-            Logger.warn(e);
+        } catch (error) {
+            this.sendError(commandEvent, argString, error);
         }
     }
 }
