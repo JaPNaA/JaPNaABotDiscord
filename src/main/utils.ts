4b6ac3d4265c9eaf256be60bf55e98cf8e295a85
JaPNaA <leonehuang100@gmail.com>
2018-12-22 11:22:55 -0500

    add toArray util

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -112,3 +112,16 @@ function tryRun(func: Function) {
 }
 
 export { tryRun };
+
+/**
+ * Converts element into element array if required
+ */
+function toArray<T> (element: T | T[]): T[] {
+    if (Array.isArray(element)) {
+        return element;
+    } else {
+        return [element];
+    }
+}
+
+export { toArray };
