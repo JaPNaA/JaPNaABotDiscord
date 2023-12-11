bb7026b78b7a2a9c4060cf0df4c99a61b498bee0
JaPNaA <japnaa@japnaa.dev>
2023-12-10 22:26:54 -0500

    Remove square brackets as formatting character

--- src/main/utils/str/removeFormattingChars.ts
+++ src/main/utils/str/removeFormattingChars.ts
@@ -1,3 +1,3 @@
 export default function removeFormattingChars(str: string): string {
-    return str.replace(/(~~|\*\*|\*|__|_)(.+?)\1/g, (_f, formattingChar, content) => formattingChar === "~~" ? "" : content)
+    return str.replace(/(~~|\*\*|\*|__|_|\[|\])(.+?)\1/g, (_f, formattingChar, content) => formattingChar === "~~" ? "" : content)
 }
